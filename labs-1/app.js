2b6718a8ba3305abf2718a6a6a8c7fb214720e36
GustavoQuezadaM <123574757+GustavoQuezadaM@users.noreply.github.com>
2023-04-02 21:05:44 -0400

    Respuesta

--- labs-1/app.js
+++ labs-1/app.js
@@ -1,4 +1,9 @@
 'use strict'
+function prefixer(prefix){
+    return (name) =>{
+        return prefix+name
+    }
+}
 const sayHiTo = prefixer('Hello ')
 const sayByeTo = prefixer('Goodbye ')
 console.log(sayHiTo('Dave')) // prints 'Hello Dave'
